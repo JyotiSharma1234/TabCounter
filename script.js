95b4ff10549d406fd3f7332c04a9d1666670c00f
Jyoti Sharma <sjyoti4321@gmail.com>
2023-05-21 17:29:01 +0530

    feat: Added icons and UI enhancement

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-chrome.storage.sync.set({key: 1});
-alert('1');
+window.addEventListener("load", () => {
+  chrome.tabs.query({ windowType: "normal" }, function (tabs) {
+    document.getElementById("tabCount").innerHTML = tabs.length;
+  });
+
+  chrome.windows.getAll({ windowTypes: ["normal"] }, function (windows) {
+    const privateWindow = windows.filter((window) => window.incognito);
+    document.getElementById("windowCount").innerHTML = windows.length;
+    document.getElementById("privateWindowCount").innerHTML =
+      privateWindow.length;
+  });
+});
